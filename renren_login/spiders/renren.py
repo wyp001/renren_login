ab7b30283b01c4aef93d716b302c664403b7ba29
wyupeng <wyupeng@linewell.com>
2020-01-06 10:44:37 +0800

    具体实现爬虫的代码

--- renren_login/spiders/renren.py
+++ renren_login/spiders/renren.py
@@ -7,5 +7,23 @@ class RenrenSpider(scrapy.Spider):
     allowed_domains = ['renren.com']
     start_urls = ['http://renren.com/']
 
-    def parse(self, response):
-        pass
+    # def parse(self, response):
+    #     pass
+
+    def start_requests(self):
+        url = "http://www.renren.com/PLogin.do"
+        data = {
+            "email":"2054510095@qq.com",
+            "password":"**********"
+        }
+        request = scrapy.FormRequest(url=url, formdata=data, callback=self.parse_page)
+        yield request
+
+    def parse_page(self,response):
+        with open("renren.html",'w',encoding='utf-8') as fp:
+            fp.write(response.text)
+
+    # def parse_profile(self, response):
+    #     pass
+
+
