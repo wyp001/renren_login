8705ef02f1fb35b78b0a1d5a8e21d679636cce4a
wyupeng <wyupeng@linewell.com>
2020-01-06 10:03:57 +0800

    Initial commit

---
+++ renren_login/spiders/renren.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class RenrenSpider(scrapy.Spider):
+    name = 'renren'
+    allowed_domains = ['renren.com']
+    start_urls = ['http://renren.com/']
+
+    def parse(self, response):
+        pass
